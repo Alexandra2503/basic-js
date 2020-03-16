e122b43f6b5f863c7c798c7f4f7fe6d15512028d
Alexandra2503 <sunh2oalex@gmail.com>
2020-03-16 13:00:24 +0300

    feat: add solution on recursive-depth and vigenere-cipher

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,10 +1,8 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
-        /*let counter=0;
-        if(arr === undefined) return false;
-        if(Array.isArray(arr)) return this.calculateDepth() */
-        throw 'Not implemented';
-    }
-    
-    // remove line with error and write your code here
+        let depth = 1;
+        if (!Array.isArray(arr)) return 0;
+        for (let i = 0; i < arr.length; i++) depth = Math.max(this.calculateDepth(arr[i]) + 1, depth);
+        return depth;
+    };
 };
